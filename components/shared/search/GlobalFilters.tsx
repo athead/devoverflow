c107fb36e6b967953a4706cfc94319dc5af1d3cf
vskill <vskill@mail.ru>
2023-12-30 19:43:15 +0300

    global search

---
+++ components/shared/search/GlobalFilters.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { GlobalSearchFilters } from "@/constants/filters";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+
+const GlobalFilters = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const typeParams = searchParams.get("type");
+
+  const [active, setActive] = useState(typeParams || "");
+
+  const handleTypeClick = (type: string) => {
+    if (active === type) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(type);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: type.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-5 px-5">
+      <p className="text-dark400_light900 body-medium">Тип</p>
+      <div className="flex gap-3">
+        {GlobalSearchFilters.map((item) => (
+          <Button
+            type="button"
+            variant={"globalTab"}
+            className={`${
+              active === item.value
+                ? "bg-primary-500 text-light-900"
+                : "bg-light-700 text-dark-400 hover:text-primary-500 dark:bg-dark-500"
+            }`}
+            key={item.value}
+            onClick={() => handleTypeClick(item.value)}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GlobalFilters;
--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,10 +1,73 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useRef, useState } from "react";
+import { GlobalSearchModal } from "./GlobalResult";
 
 const GlobalSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const searchContainerRef = useRef<HTMLInputElement>(null);
+
+  const query = searchParams.get("global");
+
+  const [search, setSearch] = useState(query || "");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: any) => {
+      if (
+        searchContainerRef.current &&
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setModalIsOpen(false);
+        setSearch("");
+      }
+    };
+    setModalIsOpen(false);
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [pathname]);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keys: ["global", "type"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+      return () => clearTimeout(delayDebounceFn);
+    }, 300);
+  }, [search, router, pathname, searchParams, query]);
+
+  const handleInputChange = (value: string) => {
+    setSearch(value);
+    if (!modalIsOpen) setModalIsOpen(true);
+    if (value === "" && modalIsOpen) setModalIsOpen(false);
+  };
+
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
+    <div
+      className="relative w-full max-w-[600px] max-lg:hidden"
+      ref={searchContainerRef}
+    >
       <div className="background-light800_darkgradient flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
@@ -15,10 +78,15 @@ const GlobalSearch = () => {
         />
         <Input
           type="text"
+          value={search}
+          onChange={(e) => {
+            handleInputChange(e.target.value);
+          }}
           placeholder="Поиск"
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+          className="paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
+      {modalIsOpen && <GlobalSearchModal />}
     </div>
   );
 };
